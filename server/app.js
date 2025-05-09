dd7bf395fc247ebc39a1bfdd2852b7a6f85bccb6
iqobv <oberskuyvitalik@gmail.com>
2025-05-10 02:13:33 +0300

    Fix: session options

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ app.use(
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: "lax",
       // sameSite: "strict",
     },
   })
