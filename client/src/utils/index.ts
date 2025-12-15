dced0410e1c27949f9b93d00b80c54d834058810
iqob <oberskuyvitalik@gmail.com>
2025-12-15 04:58:47 +0200

    fix: - fixed error with firebase initialization

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -2,6 +2,5 @@ export * from './capitalize.util';
 export * from './fcm/ensureSw.util';
 export * from './fcm/getFcmToken.util';
 export * from './fetcher.util';
-export * from './firabase.util';
 export * from './formatDateTime.util';
 export * from './formatTime.util';
